cf2b839333740542803bc70f5f9efc16bdb88b84
undframe <undframe@gmail.com>
2020-09-18 12:58:50 +0400

    Initial commit

---
+++ src/main/java/ru/istu/b1978201/KSite/KSiteApplication.java
@@ -0,0 +1,13 @@
+package ru.istu.b1978201.KSite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/istu/b1978201/KSite/KSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.istu.b1978201.KSite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
