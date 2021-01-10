c53999c72f8035f78d9b4dc32d401393e7ee6542
Роман Миниханов <roma.minihanowa@mail.ru>
2021-01-10 12:40:56 +0400

    проверяю

--- src/main/java/ru/istu/b1978201/KSite/configs/ApplicationProperties.java
+++ src/main/java/ru/istu/b1978201/KSite/configs/ApplicationProperties.java
@@ -1,10 +1,7 @@
 package ru.istu.b1978201.KSite.configs;
 
-/**
- * Класс который хранит системные данные
- */
 public class ApplicationProperties {
 
-    public static String IMAGE_IP = "http://localhost" ;
+    public static String IMAGE_IP = "http://localhost:8081";
 
 }
